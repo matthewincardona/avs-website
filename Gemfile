bb5e2fc7590e8fc971738342c1a85e7e3a2a39e1
Matthew Incardona <matthewincardona@Matthews-MBP.lan>
2023-01-23 11:42:23 -0500

    Move to postcss v2 plugin

--- Gemfile
+++ Gemfile
@@ -5,5 +5,6 @@ source "https://rubygems.org"
 # gem "rails"
 
 gem "jekyll", "~> 4.3"
-# gem "github-pages", "~> 227", group: :jekyll_plugins
-gem 'jekyll-postcss'
+group :jekyll_plugins do
+    gem 'jekyll-postcss-v2'
+  end
--- _site/tailwind.config.js
+++ _site/tailwind.config.js
@@ -1,17 +1,16 @@
 module.exports = {
-    content: [
-      './_drafts/**/*.html',
-      './_includes/**/*.html',
-      './_layouts/**/*.html',
-      './_posts/*.md',
-      './*.md',
-      './*.html',
-    ],
-    theme: {
-      theme: {
-        extend: {},
-      },
-    },
-    plugins: []
-  }
-  
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.html',
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
